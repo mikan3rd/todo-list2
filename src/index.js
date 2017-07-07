ea54f7cbc748c760be44df5ab857c74c9e231cea
Hiroki Harry Ota <mikan3rd@users.noreply.github.com>
2017-07-07 16:00:44 +0900

    Reduxの環境構築

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './components/App';
 import './css/index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './css/App.css';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+// registerServiceWorker();
