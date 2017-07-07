28a59b4137247ccb44f152ddd050955ed19159e5
Hiroki Harry Ota <mikan3rd@users.noreply.github.com>
2017-07-07 18:29:57 +0900

    タスク作成機能の実装

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from 'react'
+import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
 
 const App = () => (
   <div className="container">
     <h1>ReduxでTodoアプリ</h1>
+    <AddTodo />
     <h2><VisibleTodoList /></h2>
   </div>
 )
