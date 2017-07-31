c2540252212fcb1d5dfef5c92f155e521b42919e
Hiroki Ota <ota.h@tritrue.com>
2017-07-31 15:34:51 +0900

    Todoコンポーネントのリファクタリング

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -7,24 +7,36 @@ class Todo extends React.Component {
   static propTypes = {
     id: PropTypes.number.isRequired,
     text: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired,
+    date: PropTypes.instanceOf(Date).isRequired,
+    toggleTodo: PropTypes.func.isRequired,
+    deleteTodo: PropTypes.func.isRequired,
   }
 
   render() {
-    const link = this.props.completed? '元に戻す' : '完了！';
-    const className = this.props.completed? 'done' : 'undone';
-    const todoDelete = this.props.completed? '削除' : '';
-    const date = this.props.date
+    const {
+      id,
+      text,
+      completed,
+      date,
+      toggleTodo,
+      deleteTodo,
+    } = this.props;
+
+    const link = completed? '元に戻す' : '完了！';
+    const className = completed? 'done' : 'undone';
+    const todoDelete = completed? '削除' : '';
     var dateFixed = ''
-    if (this.props.date) {
+    if (date) {
       dateFixed = date.getHours() + '時' + date.getMinutes() + '分' + date.getSeconds() + '秒' ;
     }
     return(
       <li className={className}>
-        <p className="content">{this.props.id + 1}：{this.props.text}</p>
+        <p className="content">{id + 1}：{text}</p>
         <div className='button'>
           <span className='date'>{dateFixed}</span>　
-          <a href="" onClick={(e) => {e.preventDefault(); this.props.toggleTodo()}}>{link}</a>　
-          <a href="" onClick={(e) => {e.preventDefault(); this.props.deleteTodo()}}>{todoDelete}</a><br/>
+          <a href="" onClick={(e) => {e.preventDefault(); toggleTodo()}}>{link}</a>　
+          <a href="" onClick={(e) => {e.preventDefault(); deleteTodo()}}>{todoDelete}</a><br/>
         </div>
       </li>
     );
